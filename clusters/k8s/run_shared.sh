9dbceb9c7c8fc5248db8e82c784b3776741cefef
Charles Bricout <bricout.charles@outlook.com>
2026-01-08 14:50:45 +0000

    create restricted exp for shared parameter

--- clusters/k8s/run_shared.sh
+++ clusters/k8s/run_shared.sh
@@ -1,3 +1,3 @@
-job_yaml=$(TOTAL_EXP=192 EXP="bench-shared-64" envsubst <train-template.yaml)
+job_yaml=$(TOTAL_EXP=64 EXP="bench-shared-64-restricted" envsubst <train-template.yaml)
 job=$(echo "$job_yaml" | kubectl create -f - | awk '{print $1}')
-echo "Launched job for experience bench-shared-64"
+echo "Launched job for experience bench-shared-64-restricted"
